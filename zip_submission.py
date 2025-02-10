54016d69063f160c24ed4fb0166b1ff9717d008a
Raihan <uraihan2511@gmail.com>
2025-02-10 19:33:37 +0530

    Finally creating the zip file

--- zip_submission.py
+++ zip_submission.py
@@ -11,7 +11,7 @@ def copy_directory(src, dest):
 
 shutil.rmtree('temp_submission', ignore_errors=True)
 os.mkdir('temp_submission')
-for dir_name in ['code', 'results']:
+for dir_name in [r'Image_filtering_and_hybrid_images\code', r'Image_filtering_and_hybrid_images\results']:
   copy_directory(dir_name, '/'.join(['temp_submission', dir_name]))
-shutil.make_archive('submission', 'zip', 'temp_submission')
+shutil.make_archive('submission_insight_engineer_Sayan_Raihan', 'zip', 'temp_submission')
 shutil.rmtree('temp_submission', ignore_errors=True)
